d0e0123a256939f8b7cbb74b7c1039248534a529
iNeil Mac <neil@neildembla.com>
2025-04-20 14:21:44 -0400

    made it mobile responsive

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const { Redis } = require('@upstash/redis');
+require('dotenv').config();
 const redis = new Redis({
 	url: process.env.UPSTASH_REDIS_REST_URL,
 	token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
-require('dotenv').config();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
